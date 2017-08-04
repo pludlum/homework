8cfc62464900e292dc0a42aab7c65f34606a367f
Peter Ludlum <t.broeck@gmail.com>
2017-08-03 19:52:08 -0700

    First HW Commit

---
+++ w1d5/abs.rb
@@ -0,0 +1,66 @@
+class Stack
+  def initialize
+    @ivar = []
+  end
+
+  def add(el)
+    @ivar << el
+    nil
+  end
+
+  def remove
+    @ivar.pop
+    nil
+  end
+
+  def show
+    @ivar
+  end
+
+end
+
+class Queue
+  def initialize
+    @queue = []
+  end
+
+  def enqueue(el)
+    @queue.unshift(el)
+    true
+  end
+
+  def dequeue
+    @queue.pop
+    nil
+  end
+
+  def show
+    @queue
+  end
+end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, val)
+    @map << [key, val]
+    nil
+  end
+
+  def lookup(key)
+    pair = @map.select { |pair| pair[0] == key }
+    pair.flatten
+  end
+
+  def remove(key)
+    @maps.delete(lookup(key))
+    nil
+  end
+
+  def show
+    @map
+  end
+
+end
