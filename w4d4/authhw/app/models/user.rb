56c69728d951416837fa46c7adb9409bc5b499e7
Peter Ludlum <t.broeck@gmail.com>
2017-08-24 09:21:52 -0700

    SO MANY TYPOS

--- w4d4/authhw/app/models/user.rb
+++ w4d4/authhw/app/models/user.rb
@@ -11,25 +11,27 @@
 #
 
 class User < ApplicationRecord
-  validates :usersname, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
   validates :session_token, presence: true, uniqueness: true
-  validate :password_digest, presnce: {message: "password can't be blank"}
+  validates :password_digest, presence: {message: "password can't be blank"}
   validates :password, length: {minimum: 6, allow_nil: true}
 
   before_validation :ensure_session_token
 
+  attr_reader :password
+
 
   def password=(password)
     @password = password
-    self.psasword_digest = BCrypt::Password.create(password)
+    self.password_digest = BCrypt::Password.create(password)
   end
 
   def self.generate_session_token
-    SecrureRandom::urlsafe_base64(16)
+    SecureRandom::urlsafe_base64(16)
   end
 
   def ensure_session_token
-    User.generate_session_token unless self.session_token
+    self.session_token = User.generate_session_token unless self.session_token
   end
 
   def reset_session_token!
@@ -38,12 +40,12 @@ class User < ApplicationRecord
     self.session_token
   end
 
-  self.find_by_credentials(username, password)
+  def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
     if user && BCrypt::Password.new(user.password_digest).is_password?(password)
       return user
     else
       return nil
+    end
   end
-
 end
