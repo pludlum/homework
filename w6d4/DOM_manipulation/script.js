62494c2fb68d60e937fe756589f9c41c07e55384
Peter Ludlum <t.broeck@gmail.com>
2017-09-07 07:48:06 -0700

    w6d4

--- w6d4/DOM_manipulation/script.js
+++ w6d4/DOM_manipulation/script.js
@@ -21,6 +21,23 @@ document.addEventListener("DOMContentLoaded", () => {
   // --- your code here!
 
 
+    const handleFavoriteSubmit = (event) => {
+      event.preventDefault();
+
+      const favoriteInput = document.querySelector(".favorite-input");
+      const favorite = favoriteInput.value;
+      favoriteInput.value = "";
+
+      const newLI = document.createElement("li");
+      newLI.textContent = favorite;
+
+      const favoritesList = document.getElementById("sf-places");
+      favoritesList.appendChild(newLI);
+    };
+
+    const listSubmitButton = document.querySelector(".favorite-submit");
+    listSubmitButton.addEventListener("click", handleFavoriteSubmit);
+
 
   // adding new photos
 
