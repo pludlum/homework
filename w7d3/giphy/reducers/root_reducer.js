4958062846c27d206ba7c1fcdae496e280b1b7a2
Peter Ludlum <t.broeck@gmail.com>
2017-09-13 12:03:02 -0700

    w7d3

---
+++ w7d3/giphy/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import GiphysReducer from './giphys_reducer';
+
+import {combineReducers} from 'redux';
+
+import GiphysReducer from './giphys_reducer';
+
+export default combineReducers({
+  giphys: GiphysReducer
+});
